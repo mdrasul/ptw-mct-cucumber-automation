78bd40b0cb4689204dc2eae7a557a55503de55ea
mdsha <mdsha@DESKTOP-RBS328K.lan1>
2020-05-04 03:20:04 -0400

    This Is Md Sharifuzzaman Commit ...

---
+++ src/test/java/mct/zmisc/concepts/MdSharifTest1.java
@@ -0,0 +1,11 @@
+package mct.zmisc.concepts;
+
+public class MdSharifTest1 {
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Rasul Vai, You're the Best, Make Dua for Us!");
+	}
+
+}
